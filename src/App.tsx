bd549fa9a07c71683538511213027f899ebd6b47
Zachary Trani <trani.zach@gmail.com>
2025-02-13 16:37:04 -0500

    dist folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import axios from 'axios'
-import { useState,useEffect } from 'react'
+// import axios from 'axios'
+// import { useState,useEffect } from 'react'
 
 function App() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const baseurl: string = "http://zach-ecommerce-backend.azurewebsites.net/employees";
-    const url: string = `${baseurl}`;
+  // useEffect(() => {
+  //   const baseurl: string = "http://zach-ecommerce-backend.azurewebsites.net/employees";
+  //   const url: string = `${baseurl}`;
 
-    const getProducts = async () => {
-      try {
-        const response = await axios.get(url);
-        console.log(response.data);
-        setProducts(response.data);
-      } catch (error) {
-        console.error("Error fetching products:", error)
-      }
-    };
-    getProducts();
-  }, []);
+  //   const getProducts = async () => {
+  //     try {
+  //       const response = await axios.get(url);
+  //       console.log(response.data);
+  //       // setProducts(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching products:", error)
+  //     }
+  //   };
+  //   getProducts();
+  // }, []);
 
 
   return (
