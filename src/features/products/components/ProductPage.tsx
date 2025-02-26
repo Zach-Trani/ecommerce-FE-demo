3c55a854b386328756405f08bc955bd52619e690
Zachary Trani <trani.zach@gmail.com>
2025-02-25 20:29:19 -0500

    add: cursor rules

--- src/features/products/components/ProductPage.tsx
+++ src/features/products/components/ProductPage.tsx
@@ -32,8 +32,8 @@ const ProductPage = () => {
       const amountInCents = Math.round(selectedProduct.price * 100);
 
       const response = await fetch(
-        "https://zach-ecommerce-backend.azurewebsites.net/product/v1/checkout",
-        // "http://localhost:9191/product/v1/checkout",
+        // "https://zach-ecommerce-backend.azurewebsites.net/product/v1/checkout",
+        "http://localhost:9191/product/v1/checkout",
         {
           method: "POST",
           credentials: "include",
